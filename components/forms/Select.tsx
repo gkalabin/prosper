cf8303d8a59312bab52cc5e0954af9be9bc157b2
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:24 +0000

    Align Select inputs styles with text inputs

--- components/forms/Select.tsx
+++ components/forms/Select.tsx
@@ -10,7 +10,7 @@ export const Select = (
       className={classNames(
         className,
         props.disabled ? "opacity-30" : "",
-        "rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+        "rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
       )}
     >
       {props.children}
