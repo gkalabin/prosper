1b0c1fa99b54256a99f3157a585d11684e0d497a
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:30 +0000

    Handle exchange failures in trip stats

--- app/trips/[tripName]/TripDetails.tsx
+++ app/trips/[tripName]/TripDetails.tsx
@@ -1,33 +1,38 @@
 "use client";
 import { Trip as DBTrip } from "@prisma/client";
+import { CurrencyExchangeFailed } from "app/stats/CurrencyExchangeFailed";
+import {
+  NotConfiguredYet,
+  isFullyConfigured,
+} from "components/NotConfiguredYet";
 import {
   ChildCategoryFullAmountChart,
   ChildCategoryOwnShareChart,
 } from "components/charts/CategoryPie";
 import {
-  isFullyConfigured,
-  NotConfiguredYet,
-} from "components/NotConfiguredYet";
-import {
   SortableTransactionsList,
   SortingMode,
 } from "components/transactions/SortableTransactionsList";
 import { AnchorLink } from "components/ui/anchors";
+import { AmountWithCurrency } from "lib/AmountWithCurrency";
 import {
   AllDatabaseDataContextProvider,
   useAllDatabaseDataContext,
 } from "lib/ClientSideModel";
 import { useDisplayCurrency } from "lib/displaySettings";
 import { AllDatabaseData } from "lib/model/AllDatabaseDataModel";
+import { Trip, tripModelFromDB } from "lib/model/Trip";
+import { Income } from "lib/model/transaction/Income";
 import {
   Expense,
+  Transaction,
   isExpense,
   isIncome,
 } from "lib/model/transaction/Transaction";
-import { amountOwnShare } from "lib/model/transaction/amounts";
-import { amountAllParties } from "lib/model/transaction/amounts";
-import { Income } from "lib/model/transaction/Income";
-import { Trip, tripModelFromDB } from "lib/model/Trip";
+import {
+  amountAllParties,
+  amountOwnShare,
+} from "lib/model/transaction/amounts";
 
 function NonEmptyTripDetails(props: { trip: Trip }) {
   const { transactions: allTransactions } = useAllDatabaseDataContext();
@@ -37,21 +42,42 @@ function NonEmptyTripDetails(props: { trip: Trip }) {
     .filter((tx): tx is Income | Expense => isIncome(tx) || isExpense(tx))
     .filter((tx) => tx.tripId == props.trip.id);
 
-  const fullAmount = transactions
-    .map((t) =>
-      amountAllParties(t, displayCurrency, bankAccounts, stocks, exchange),
-    )
-    .reduce((a, b) => a.add(b));
-  const ownAmount = transactions
-    .map((t) =>
-      amountOwnShare(t, displayCurrency, bankAccounts, stocks, exchange),
-    )
-    .reduce((a, b) => a.add(b));
-
+  const failedToExchange: Transaction[] = [];
+  let fullAmount = AmountWithCurrency.zero(displayCurrency);
+  let ownAmount = AmountWithCurrency.zero(displayCurrency);
+  for (const t of transactions) {
+    let failed = false;
+    const all = amountAllParties(
+      t,
+      displayCurrency,
+      bankAccounts,
+      stocks,
+      exchange,
+    );
+    if (all) {
+      fullAmount = fullAmount.add(all);
+    } else {
+      failed = true;
+      failedToExchange.push(t);
+    }
+    const own = amountOwnShare(
+      t,
+      displayCurrency,
+      bankAccounts,
+      stocks,
+      exchange,
+    );
+    if (own) {
+      ownAmount = ownAmount.add(own);
+    } else if (!failed) {
+      failedToExchange.push(t);
+    }
+  }
   return (
     <div>
       <AnchorLink href="/trips">Back to all trips</AnchorLink>
       <h1 className="text-xl leading-7">{props.trip.name}</h1>
+      <CurrencyExchangeFailed failedTransactions={failedToExchange} />
       <div>Gross amount: {fullAmount.format()}</div>
       <div>
         Net amount, own share only: {ownAmount.format()} (
