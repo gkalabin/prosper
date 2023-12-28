2bdc295dce2db04f06f849d860c3ea3ad948c295
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:30 +0000

    Handle non exchangeable tx in the monthly charts

--- components/charts/MonthlySum.tsx
+++ components/charts/MonthlySum.tsx
@@ -1,98 +1,92 @@
+import { CurrencyExchangeFailed } from "app/stats/CurrencyExchangeFailed";
 import { Interval, eachMonthOfInterval } from "date-fns";
 import ReactEcharts from "echarts-for-react";
-import { useAllDatabaseDataContext } from "lib/ClientSideModel";
+import { AmountWithCurrency } from "lib/AmountWithCurrency";
+import {
+  StockAndCurrencyExchange,
+  useAllDatabaseDataContext,
+} from "lib/ClientSideModel";
 import { defaultMonthlyMoneyChart } from "lib/charts";
 import { useDisplayCurrency } from "lib/displaySettings";
-import {
-  Expense,
-} from "lib/model/transaction/Transaction";
-import { amountOwnShare } from "lib/model/transaction/amounts";
-import { amountAllParties } from "lib/model/transaction/amounts";
+import { BankAccount } from "lib/model/BankAccount";
+import { Currency } from "lib/model/Currency";
+import { Stock } from "lib/model/Stock";
 import { Income } from "lib/model/transaction/Income";
+import { Expense, Transaction } from "lib/model/transaction/Transaction";
+import {
+  amountAllParties,
+  amountOwnShare,
+} from "lib/model/transaction/amounts";
 import { MoneyTimeseries } from "lib/util/Timeseries";
 
-export function MonthlyOwnShare({
-  transactions,
-  duration,
-  title,
-}: {
+export function MonthlyOwnShare(props: {
   transactions: (Expense | Income)[];
   duration: Interval;
   title: string;
 }) {
-  const displayCurrency = useDisplayCurrency();
-  const { bankAccounts, stocks, exchange } = useAllDatabaseDataContext();
-  const months = eachMonthOfInterval(duration);
-  const data = new MoneyTimeseries(displayCurrency);
-  for (const t of transactions) {
-    const amount = amountOwnShare(
-      t,
-      displayCurrency,
-      bankAccounts,
-      stocks,
-      exchange
-    );
-    data.append(t.timestampEpoch, amount);
-  }
-  return (
-    <ReactEcharts
-      notMerge
-      option={{
-        ...defaultMonthlyMoneyChart(displayCurrency, duration),
-        title: {
-          text: title,
-        },
-        series: [
-          {
-            type: "bar",
-            name: title,
-            data: data.monthRoundDollars(months),
-          },
-        ],
-      }}
-    />
-  );
+  return <Monthly {...props} amountFn={amountOwnShare} />;
 }
 
-export function MonthlyAllParties({
+export function MonthlyAllParties(props: {
+  transactions: (Expense | Income)[];
+  duration: Interval;
+  title: string;
+}) {
+  return <Monthly {...props} amountFn={amountAllParties} />;
+}
+
+function Monthly({
   transactions,
+  amountFn,
   duration,
   title,
 }: {
   transactions: (Expense | Income)[];
   duration: Interval;
   title: string;
+  amountFn: (
+    t: Expense | Income,
+    target: Currency,
+    bankAccounts: BankAccount[],
+    stocks: Stock[],
+    exchange: StockAndCurrencyExchange,
+  ) => AmountWithCurrency | undefined;
 }) {
   const displayCurrency = useDisplayCurrency();
   const { bankAccounts, stocks, exchange } = useAllDatabaseDataContext();
   const months = eachMonthOfInterval(duration);
   const data = new MoneyTimeseries(displayCurrency);
+  // TODO: validate that transactions can be exchanged on the page level
+  // and only pass down the exchangeable ones as displaying the same
+  // warning for each chart is noisy.
+  const failedToExchange: Transaction[] = [];
   for (const t of transactions) {
-    const amount = amountAllParties(
-      t,
-      displayCurrency,
-      bankAccounts,
-      stocks,
-      exchange
-    );
+    const amount = amountFn(t, displayCurrency, bankAccounts, stocks, exchange);
+    if (!amount) {
+      failedToExchange.push(t);
+      continue;
+    }
     data.append(t.timestampEpoch, amount);
   }
   return (
-    <ReactEcharts
-      notMerge
-      option={{
-        ...defaultMonthlyMoneyChart(displayCurrency, duration),
-        title: {
-          text: title,
-        },
-        series: [
-          {
-            type: "bar",
-            name: title,
-            data: data.monthRoundDollars(months),
+    <>
+      <CurrencyExchangeFailed failedTransactions={failedToExchange} />
+      <ReactEcharts
+        notMerge
+        option={{
+          ...defaultMonthlyMoneyChart(displayCurrency, duration),
+          title: {
+            text: title,
           },
-        ],
-      }}
-    />
+          series: [
+            {
+              type: "bar",
+              name: title,
+              data: data.monthRoundDollars(months),
+            },
+          ],
+        }}
+      />
+    </>
   );
 }
