61d06c2a452f29c2ba277996295fe595f98a8a7c
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:30 +0000

    Category pie handling of exchange failures

--- components/charts/CategoryPie.tsx
+++ components/charts/CategoryPie.tsx
@@ -1,14 +1,21 @@
+import { CurrencyExchangeFailed } from "app/stats/CurrencyExchangeFailed";
 import { AmountWithCurrency } from "lib/AmountWithCurrency";
-import { useAllDatabaseDataContext } from "lib/ClientSideModel";
+import {
+  StockAndCurrencyExchange,
+  useAllDatabaseDataContext,
+} from "lib/ClientSideModel";
 import { defaultPieChartOptions } from "lib/charts";
 import { useDisplayCurrency } from "lib/displaySettings";
-import { mustFindCategory } from "lib/model/Category";
-import {
-  Expense,
-} from "lib/model/transaction/Transaction";
-import { amountOwnShare } from "lib/model/transaction/amounts";
-import { amountAllParties } from "lib/model/transaction/amounts";
+import { BankAccount } from "lib/model/BankAccount";
+import { Category, mustFindCategory } from "lib/model/Category";
+import { Currency } from "lib/model/Currency";
+import { Stock } from "lib/model/Stock";
 import { Income } from "lib/model/transaction/Income";
+import { Expense, Transaction } from "lib/model/transaction/Transaction";
+import {
+  amountAllParties,
+  amountOwnShare,
+} from "lib/model/transaction/amounts";
 import { AppendMap, currencyAppendMap } from "lib/util/AppendingMap";
 import dynamic from "next/dynamic";
 
@@ -21,23 +28,14 @@ export function TopLevelCategoryOwnShareChart({
   transactions: (Expense | Income)[];
   title: string;
 }) {
-  const displayCurrency = useDisplayCurrency();
-  const { categories, bankAccounts, stocks, exchange } =
-    useAllDatabaseDataContext();
-  const data = currencyAppendMap<number>(displayCurrency);
-  for (const t of transactions) {
-    const category = mustFindCategory(t.categoryId, categories);
-    const cid = category.root().id();
-    const amount = amountOwnShare(
-      t,
-      displayCurrency,
-      bankAccounts,
-      stocks,
-      exchange,
-    );
-    data.append(cid, amount);
-  }
-  return <ByCategoryChart title={title} data={data} />;
+  return (
+    <ByCategoryChart
+      title={title}
+      transactions={transactions}
+      categoryFn={rootCategoryId}
+      amountFn={amountOwnShare}
+    />
+  );
 }
 
 export function TopLevelCategoryFullAmountChart({
@@ -47,23 +45,14 @@ export function TopLevelCategoryFullAmountChart({
   transactions: (Expense | Income)[];
   title: string;
 }) {
-  const displayCurrency = useDisplayCurrency();
-  const { categories, bankAccounts, stocks, exchange } =
-    useAllDatabaseDataContext();
-  const data = currencyAppendMap<number>(displayCurrency);
-  for (const t of transactions) {
-    const category = mustFindCategory(t.categoryId, categories);
-    const cid = category.root().id();
-    const amount = amountAllParties(
-      t,
-      displayCurrency,
-      bankAccounts,
-      stocks,
-      exchange,
-    );
-    data.append(cid, amount);
-  }
-  return <ByCategoryChart title={title} data={data} />;
+  return (
+    <ByCategoryChart
+      title={title}
+      transactions={transactions}
+      categoryFn={rootCategoryId}
+      amountFn={amountAllParties}
+    />
+  );
 }
 
 export function ChildCategoryOwnShareChart({
@@ -73,23 +62,14 @@ export function ChildCategoryOwnShareChart({
   transactions: (Expense | Income)[];
   title: string;
 }) {
-  const displayCurrency = useDisplayCurrency();
-  const { categories, bankAccounts, stocks, exchange } =
-    useAllDatabaseDataContext();
-  const data = currencyAppendMap<number>(displayCurrency);
-  for (const t of transactions) {
-    const category = mustFindCategory(t.categoryId, categories);
-    const cid = category.id();
-    const amount = amountOwnShare(
-      t,
-      displayCurrency,
-      bankAccounts,
-      stocks,
-      exchange,
-    );
-    data.append(cid, amount);
-  }
-  return <ByCategoryChart title={title} data={data} />;
+  return (
+    <ByCategoryChart
+      title={title}
+      transactions={transactions}
+      categoryFn={leafCategoryId}
+      amountFn={amountOwnShare}
+    />
+  );
 }
 
 export function ChildCategoryFullAmountChart({
@@ -99,51 +79,106 @@ export function ChildCategoryFullAmountChart({
   transactions: (Expense | Income)[];
   title: string;
 }) {
-  const displayCurrency = useDisplayCurrency();
-  const { categories, bankAccounts, stocks, exchange } =
-    useAllDatabaseDataContext();
-  const data = currencyAppendMap<number>(displayCurrency);
+  return (
+    <ByCategoryChart
+      title={title}
+      transactions={transactions}
+      categoryFn={leafCategoryId}
+      amountFn={amountAllParties}
+    />
+  );
+}
+
+function groupTransactions<T>({
+  currency,
+  transactions,
+  groupFn,
+  amountFn,
+}: {
+  currency: Currency;
+  transactions: (Expense | Income)[];
+  groupFn: (t: Transaction) => T;
+  amountFn: (t: Expense | Income, currency: Currency) => AmountWithCurrency;
+}): AppendMap<T, AmountWithCurrency> {
+  const data = currencyAppendMap<T>(currency);
   for (const t of transactions) {
-    const category = mustFindCategory(t.categoryId, categories);
-    const cid = category.id();
-    const amount = amountAllParties(
-      t,
-      displayCurrency,
-      bankAccounts,
-      stocks,
-      exchange,
-    );
-    data.append(cid, amount);
+    const k = groupFn(t);
+    const amount = amountFn(t, currency);
+    data.append(k, amount);
   }
-  return <ByCategoryChart title={title} data={data} />;
+  return data;
+}
+
+function rootCategoryId(t: Transaction, categories: Category[]): number {
+  const category = mustFindCategory(t.categoryId, categories);
+  return category.root().id();
+}
+
+function leafCategoryId(t: Transaction, categories: Category[]): number {
+  const category = mustFindCategory(t.categoryId, categories);
+  return category.id();
 }
 
 function ByCategoryChart({
-  data,
+  transactions,
   title,
+  categoryFn,
+  amountFn,
 }: {
-  data: AppendMap<number, AmountWithCurrency>;
+  transactions: (Expense | Income)[];
   title: string;
+  categoryFn: (t: Transaction, categories: Category[]) => number;
+  amountFn: (
+    t: Expense | Income,
+    target: Currency,
+    bankAccounts: BankAccount[],
+    stocks: Stock[],
+    exchange: StockAndCurrencyExchange,
+  ) => AmountWithCurrency | undefined;
 }) {
-  const { categories } = useAllDatabaseDataContext();
+  const { categories, bankAccounts, stocks, exchange } =
+    useAllDatabaseDataContext();
+  const displayCurrency = useDisplayCurrency();
+  const failedToExchange: Transaction[] = [];
+  const groupFn = (t: Transaction): number => categoryFn(t, categories);
+  const amountFnWithModel = (
+    t: Expense | Income,
+    currency: Currency,
+  ): AmountWithCurrency => {
+    const amount = amountFn(t, currency, bankAccounts, stocks, exchange);
+    if (!amount) {
+      failedToExchange.push(t);
+      return AmountWithCurrency.zero(currency);
+    }
+    return amount;
+  };
+  const data = groupTransactions<number>({
+    currency: displayCurrency,
+    transactions,
+    groupFn,
+    amountFn: amountFnWithModel,
+  });
   return (
-    <ReactEcharts
-      notMerge
-      option={{
-        ...defaultPieChartOptions(),
-        title: {
-          text: title,
-        },
-        series: [
-          {
-            type: "pie",
-            data: [...data.entries()].map(([cid, amount]) => ({
-              name: mustFindCategory(cid, categories).nameWithAncestors(),
-              value: amount.dollar(),
-            })),
+    <>
+      <CurrencyExchangeFailed failedTransactions={failedToExchange} />
+      <ReactEcharts
+        notMerge
+        option={{
+          ...defaultPieChartOptions(),
+          title: {
+            text: title,
           },
-        ],
-      }}
-    />
+          series: [
+            {
+              type: "pie",
+              data: [...data.entries()].map(([cid, amount]) => ({
+                name: mustFindCategory(cid, categories).nameWithAncestors(),
+                value: amount.dollar(),
+              })),
+            },
+          ],
+        }}
+      />
+    </>
   );
 }
