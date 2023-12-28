c39a10e2678861caebae7176c5bd173e34d5de2d
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:30 +0000

    Bring back loading rates and stock quotes

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,5 +1,7 @@
 import { OverviewPage } from "app/overview/OverviewPage";
 import { DB, fetchAllDatabaseData } from "lib/db";
+import { addLatestExchangeRates } from "lib/exchangeRatesBackfill";
+import { addLatestStockQuotes } from "lib/stockQuotesBackfill";
 import { getUserId } from "lib/user";
 import { Metadata } from "next";
 
@@ -10,6 +12,10 @@ export const metadata: Metadata = {
 export default async function Page() {
   const userId = await getUserId();
   const db = new DB({ userId });
+  await Promise.all([
+    await addLatestExchangeRates(),
+    await addLatestStockQuotes(),
+  ]);
   const data = await fetchAllDatabaseData(db);
   return <OverviewPage dbData={data} />;
 }
