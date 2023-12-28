f27cbf17b5e6a099843890bc0249796d9e7c0b24
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:27 +0000

    Remove old migration file

--- migration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /opt/prosper
-npx prisma db push
-sudo mysql prosperdb -e 'select t.id from PersonalExpense e join Transaction t on e.transactionId=t.id where ownShareAmountCents != amountCents and t.userId=1' | grep -v id | sudo xargs -I {} mysql prosperdb -e 'update PersonalExpense set otherPartyName="Kristina" where transactionId={}'
-sudo mysql prosperdb -e 'select t.id from PersonalExpense e join Transaction t on e.transactionId=t.id where ownShareAmountCents != amountCents and t.userId=2' | grep -v id | sudo xargs -I {} mysql prosperdb -e 'update PersonalExpense set otherPartyName="Grishka" where transactionId={}'
-sudo mysql prosperdb -e 'update Income set payer=vendor'
