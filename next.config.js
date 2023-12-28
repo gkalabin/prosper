f7e074a231a64d74e5652e84a74166706fb39613
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:29 +0000

    Try removing ignoring watch options

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@
  */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.watchOptions = {
-      ignored: /node_modules/,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
