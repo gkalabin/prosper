0182cb2c3107bc11ed2e898aa869162e2bb1cbb2
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:27 +0000

    Use async/await for fetching balance from nordingen API

--- lib/openbanking/nordigen/balance.tsx
+++ lib/openbanking/nordigen/balance.tsx
@@ -1,32 +1,33 @@
 import { ExternalAccountMapping, NordigenToken } from "@prisma/client";
 import { AccountBalance } from "lib/openbanking/interface";
 
-export function fetchBalance(
+export async function fetchBalance(
   token: NordigenToken,
   mapping: ExternalAccountMapping
 ): Promise<AccountBalance> {
-  return fetch(
+  const response = await fetch(
     `https://ob.nordigen.com/api/v2/accounts/${mapping.externalAccountId}/balances/`,
     {
       method: "GET",
       headers: { Authorization: `Bearer ${token.access}` },
     }
-  )
-    .then((r) => r.json())
-    .then((r) => {
-      if (!r.balances?.length) {
-        console.warn("No balance found", JSON.stringify(r, null, 2));
-        return;
-      }
-      for (const { balanceType, balanceAmount } of r.balances) {
-        if (balanceType === "interimAvailable" || balanceType === "expected") {
-          return {
-            balanceCents: Math.round(balanceAmount.amount * 100),
-            internalAccountId: mapping.internalAccountId,
-          };
-        }
-      }
-      console.warn("No balance found", JSON.stringify(r, null, 2));
-      return;
-    });
+  );
+  if (response.status !== 200) {
+    throw new Error(
+      `Refresh token for ${token.bankId} failed (code ${
+        response.status
+      }): ${await response.text()}`
+    );
+  }
+  const r = await response.json();
+  const balances = r.balances ?? [];
+  for (const { balanceType, balanceAmount } of balances) {
+    if (balanceType === "interimAvailable" || balanceType === "expected") {
+      return {
+        balanceCents: Math.round(balanceAmount.amount * 100),
+        internalAccountId: mapping.internalAccountId,
+      };
+    }
+  }
+  throw new Error(`no balance found: ${JSON.stringify(r, null, 2)}`);
 }
