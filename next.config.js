2c98aa535fd7ed55559ce0f09017e11aa33090c9
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:23 +0000

    Fix too many files being watched error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@
  */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: /node_modules/,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
