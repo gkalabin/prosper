59bd2b8b8fb03ff3d53f85cfa0f0fb3406c50138
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-28 17:04:27 +0000

    Log timings for getServerSideProps

--- lib/ServerSideDB.tsx
+++ lib/ServerSideDB.tsx
@@ -37,6 +37,9 @@ const jsonEncodingHacks = (key: string, value) => {
 export const allDbDataProps: GetServerSideProps<AllDatabaseData> = async (
   context
 ) => {
+  const timingLabelSuffix = new Date().getTime();
+  console.time("allDbDataProps:" + timingLabelSuffix);
+  console.time("allDbDataProps:getServerSession:" + timingLabelSuffix);
   const session = await getServerSession(context.req, context.res, authOptions);
   if (!session) {
     return {
@@ -46,13 +49,24 @@ export const allDbDataProps: GetServerSideProps<AllDatabaseData> = async (
       },
     };
   }
+  console.timeEnd("allDbDataProps:getServerSession:" + timingLabelSuffix);
+  console.time("allDbDataProps:exchangeRates:" + timingLabelSuffix);
   await Promise.all([addLatestExchangeRates(), addLatestStockQuotes()]).catch(
     (reason) => {
       console.warn("Failed to update rates", reason);
     }
   );
+  console.timeEnd("allDbDataProps:exchangeRates:" + timingLabelSuffix);
+  console.time("allDbDataProps:fetchAllDatabaseData:" + timingLabelSuffix);
   const db = await DB.fromContext(context);
   const dbData = await fetchAllDatabaseData(db);
+  console.timeEnd("allDbDataProps:fetchAllDatabaseData:" + timingLabelSuffix);
+  console.time("allDbDataProps:addSession:" + timingLabelSuffix);
   const props = Object.assign(dbData, { session });
-  return JSON.parse(JSON.stringify({ props }, jsonEncodingHacks));
+  console.timeEnd("allDbDataProps:addSession:" + timingLabelSuffix);
+  console.time("allDbDataProps:jsonify:" + timingLabelSuffix);
+  const result = JSON.parse(JSON.stringify({ props }, jsonEncodingHacks));
+  console.timeEnd("allDbDataProps:jsonify:" + timingLabelSuffix);
+  console.timeEnd("allDbDataProps:" + timingLabelSuffix);
+  return result;
 };
