a55b2818b86d9d22cb89fb5013062e4a5c06d93f
Gregory Kalabin <grigory.kalabin@gmail.com>
2023-12-30 23:39:34 +0000

    Fix trip model's typescript strict mode errors

--- lib/model/Trip.tsx
+++ lib/model/Trip.tsx
@@ -1,55 +1,19 @@
 import { Trip as DBTrip } from "@prisma/client";
 
-
 export type Trip = {
   id: number;
   name: string;
-  destination?: string;
-  startEpoch?: number;
-  endEpoch?: number;
+  destination: string | null;
+  startEpoch: number | null;
+  endEpoch: number | null;
 };
-
 
 export function tripModelFromDB(init: DBTrip): Trip {
   return {
     id: init.id,
     name: init.name,
     destination: init.destination,
-    startEpoch: init.start?.getTime(),
-    endEpoch: init.end?.getTime(),
+    startEpoch: init.start?.getTime() ?? null,
+    endEpoch: init.end?.getTime() ?? null,
   };
 }
-
-// export class Trip {
-//   private readonly _id: number;
-//   private readonly _name: string;
-//   private readonly _destination: string;
-//   private readonly _start: Date;
-//   private readonly _end: Date;
-//   private readonly dbValue: DBTrip;
-
-//   public constructor(init: DBTrip) {
-//     this.dbValue = init;
-//     this._id = init.id;
-//     this._name = init.name;
-//     this._destination = init.destination;
-//     this._start = init.start;
-//     this._end = init.end;
-//   }
-
-//   id() {
-//     return this._id;
-//   }
-//   name() {
-//     return this._name;
-//   }
-//   destination() {
-//     return this._destination;
-//   }
-//   start() {
-//     return this._start;
-//   }
-//   end() {
-//     return this._end;
-//   }
-// }
