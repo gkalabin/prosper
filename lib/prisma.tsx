f20f480b931addda9a0c244ab91eb4020f48e36c
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-01-17 09:14:24 +0000

    Use the latest recommended prisma client instantiation

--- lib/prisma.tsx
+++ lib/prisma.tsx
@@ -1,15 +1,18 @@
 import {PrismaClient} from '@prisma/client';
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 declare global {
-  // Global is special and let/const don't work there.
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma;
+}
