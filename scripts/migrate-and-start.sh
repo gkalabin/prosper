28aabe7ffcf43f6a4958847f4def42fb6da47576
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-01-18 19:17:12 +0000

    Construct DB_URL on start in docker

--- scripts/migrate-and-start.sh
+++ scripts/migrate-and-start.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ] && [ -n "$DB_USER" ] && [ -n "$DB_PASSWORD" ] && [ -n "$DB_NAME" ]; then
+  export DB_URL="mysql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
+fi
+
 npx prisma migrate deploy --schema=/app/prisma/schema.prisma
 # server.js is created by next build from the standalone output
 # https://nextjs.org/docs/pages/api-reference/next-config-js/output
