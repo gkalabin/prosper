d8491e25af06d4715f5f9a588e61f00dde56fc07
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-01-19 23:29:26 +0000

    Set NEXTAUTH_URL inside docker container

--- scripts/migrate-and-start.sh
+++ scripts/migrate-and-start.sh
@@ -4,6 +4,13 @@ if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ] && [ -n "$DB_USER" ] && [ -n "$DB_PASS
   export DB_URL="mysql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 fi
 
+if [ -n "$PUBLIC_APP_URL" ]; then
+  # It is impossible to set NEXTAUTH_URL at runtime (https://github.com/nextauthjs/next-auth/pull/1168).
+  # There is already PUBLIC_APP_URL which has the public host (e.g. https://prosper.com), make NEXTAUTH_URL to use the same value.
+  # Substitution is not an option as docker run --env-file does not support it.
+  export NEXTAUTH_URL="${PUBLIC_APP_URL}"
+fi
+
 npx prisma migrate deploy --schema=/app/prisma/schema.prisma
 # server.js is created by next build from the standalone output
 # https://nextjs.org/docs/pages/api-reference/next-config-js/output
