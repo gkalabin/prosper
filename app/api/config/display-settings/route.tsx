862227bac81de2071ef99e6592f334a34da794a7
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-01-29 19:24:34 +0000

    Validate display settings API input

--- app/api/config/display-settings/route.tsx
+++ app/api/config/display-settings/route.tsx
@@ -1,3 +1,5 @@
+import {DB} from 'lib/db';
+import {findByCode} from 'lib/model/Currency';
 import {DisplaySettingsFormValues} from 'lib/model/api/DisplaySettingsConfig';
 import prisma from 'lib/prisma';
 import {getUserId} from 'lib/user';
@@ -5,11 +7,28 @@ import {NextRequest, NextResponse} from 'next/server';
 
 export async function PUT(request: NextRequest): Promise<Response> {
   const userId = await getUserId();
-  // TODO: validate that the currency code is valid and category ids belong to the user.
   const {
     displayCurrencyCode,
     excludeCategoryIdsInStats,
   }: DisplaySettingsFormValues = await request.json();
+  const currency = findByCode(displayCurrencyCode);
+  if (!currency) {
+    return new Response(`Unknown currency ${displayCurrencyCode}`, {
+      status: 400,
+    });
+  }
+  const db = new DB({userId});
+  const allCategories = await db.categoryFindMany();
+  const known = new Set<number>(allCategories.map(c => c.id));
+  const unknownCategories = excludeCategoryIdsInStats.filter(known.has);
+  if (unknownCategories.length > 0) {
+    return new Response(
+      `Following categories are not found: ${unknownCategories}`,
+      {
+        status: 400,
+      }
+    );
+  }
   const result = await prisma.displaySettings.update({
     data: {
       displayCurrencyCode,
