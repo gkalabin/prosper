09edaf9b37cc7ae58b65669b59453aa44355b580
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-08-12 15:10:00 +0100

    Remove commented out unused code

--- src/lib/model/Tag.tsx
+++ src/lib/model/Tag.tsx
@@ -11,21 +11,3 @@ export function tagModelFromDB(init: DBTag): Tag {
     name: init.name,
   };
 }
-
-// export class Tag {
-//   private readonly _id: number;
-//   private readonly _name: string;
-//   private readonly _dbValue: DBTag;
-
-//   public constructor(init: DBTag) {
-//     this._dbValue = init;
-//     this._id = init.id;
-//     this._name = init.name;
-//   }
-//   id() {
-//     return this._id;
-//   }
-//   name() {
-//     return this._name;
-//   }
-// }
