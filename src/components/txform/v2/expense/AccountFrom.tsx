c9bacfb8e05a761f60bed1896761226f03b2021e
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-08-13 15:33:54 +0100

    Adopt shadcn form for AccountFrom inputs

--- src/components/txform/v2/expense/AccountFrom.tsx
+++ src/components/txform/v2/expense/AccountFrom.tsx
@@ -1,18 +1,21 @@
-import {Select} from '@/components/forms/Select';
 import {TransactionFormSchema} from '@/components/txform/v2/types';
-import {ButtonLink} from '@/components/ui/buttons';
+import {Button} from '@/components/ui/button';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {Select} from '@/components/ui/html-select';
 import {useAllDatabaseDataContext} from '@/lib/context/AllDatabaseDataContext';
 import {useDisplayBankAccounts} from '@/lib/model/AllDatabaseDataModel';
 import {fullAccountName} from '@/lib/model/BankAccount';
 import {useFormContext} from 'react-hook-form';
 
 export function AccountFrom() {
-  const {
-    register,
-    formState: {isSubmitting},
-    setValue,
-    watch,
-  } = useFormContext<TransactionFormSchema>();
+  const {formState, setValue, watch, control} =
+    useFormContext<TransactionFormSchema>();
   const share = watch('expense.shareType');
   const paidSelf =
     'PAID_SELF_SHARED' == share || 'PAID_SELF_NOT_SHARED' == share;
@@ -22,38 +25,40 @@ export function AccountFrom() {
     return <></>;
   }
   return (
-    <div className="col-span-6">
-      <label
-        htmlFor="accountId"
-        className="block text-sm font-medium text-gray-700"
-      >
-        I paid from
-      </label>
-      <Select
-        className="block w-full"
-        disabled={isSubmitting}
-        {...register('expense.accountId', {
-          valueAsNumber: true,
-        })}
-      >
-        {accounts.map(x => (
-          <option key={x.id} value={x.id}>
-            {fullAccountName(x, banks)}
-          </option>
-        ))}
-      </Select>
-
-      <div className="text-xs">
-        or{' '}
-        <ButtonLink
-          onClick={() => {
-            setValue('expense.shareType', 'PAID_OTHER_OWED');
-          }}
-        >
-          someone else paid for this expense
-        </ButtonLink>
-        .
-      </div>
-    </div>
+    <FormField
+      control={control}
+      name="expense.accountId"
+      render={({field}) => (
+        <FormItem className="col-span-6">
+          <FormLabel>I paid from</FormLabel>
+          <FormControl>
+            <Select
+              {...field}
+              value={field.value?.toString()}
+              onChange={e => field.onChange(parseInt(e.target.value, 10))}
+            >
+              {accounts.map(x => (
+                <option key={x.id} value={x.id}>
+                  {fullAccountName(x, banks)}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormMessage />
+          <div className="text-xs">
+            or{' '}
+            <Button
+              onClick={() => setValue('expense.shareType', 'PAID_OTHER_OWED')}
+              variant="link"
+              size="inherit"
+              disabled={formState.isSubmitting}
+            >
+              someone else paid for this expense
+            </Button>
+            .
+          </div>
+        </FormItem>
+      )}
+    />
   );
 }
---
+++ src/components/ui/html-select.tsx
@@ -0,0 +1,28 @@
+import {cn} from '@/lib/utils';
+import * as React from 'react';
+
+export interface SelectProps
+  extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+
+// TODO: move the styles to a common place to avoid duplication with Input.
+const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({className, ...props}, ref) => {
+    return (
+      <select
+        className={cn(
+          'border-Select flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm ring-offset-background',
+          'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+          'disabled:cursor-not-allowed disabled:opacity-50',
+          className
+        )}
+        ref={ref}
+        {...props}
+      >
+        {props.children}
+      </select>
+    );
+  }
+);
+Select.displayName = 'Select';
+
+export {Select};
