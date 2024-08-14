8c80a4c3eff73aa4cfe578c5b8f637bc61cbd1bb
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-08-14 23:46:47 +0100

    Use shadcn's components for category select

--- src/components/txform/v2/expense/inputs/Category.tsx
+++ src/components/txform/v2/expense/inputs/Category.tsx
@@ -1,93 +1,181 @@
-import {undoTailwindInputStyles} from '@/components/forms/Select';
 import {TransactionFormSchema} from '@/components/txform/v2/types';
+import {Button} from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {Popover, PopoverContent, PopoverTrigger} from '@/components/ui/popover';
 import {uniqMostFrequent} from '@/lib/collections';
 import {useAllDatabaseDataContext} from '@/lib/context/AllDatabaseDataContext';
 import {
   Category as CategoryModel,
+  CategoryTree,
   getNameWithAncestors,
   immediateChildren,
   makeCategoryTree,
   mustFindCategory,
 } from '@/lib/model/Category';
 import {Transaction, isExpense} from '@/lib/model/transaction/Transaction';
-import {notEmpty} from '@/lib/util/util';
+import {cn} from '@/lib/utils';
+import {CheckIcon, ChevronUpDownIcon} from '@heroicons/react/24/outline';
 import {differenceInMonths} from 'date-fns';
-import {useMemo} from 'react';
-import {Controller, useFormContext} from 'react-hook-form';
-import ReactSelect from 'react-select';
+import {useCallback, useMemo, useState} from 'react';
+import {useFormContext} from 'react-hook-form';
 
 const MAX_MOST_FREQUENT = 5;
 
 export function Category() {
-  const {control, getValues} = useFormContext<TransactionFormSchema>();
-  const {categories, transactions} = useAllDatabaseDataContext();
-  const vendor = getValues('expense.vendor') ?? '';
-  const mostFrequentIds = useMemo(
-    () => mostFrequentCategories(transactions, vendor),
-    [transactions, vendor]
-  );
-  const mostFrequent = mostFrequentIds
-    .map(id => categories.find(c => c.id == id))
-    .filter(notEmpty);
-  const tree = makeCategoryTree(categories);
-  const categoriesWithoutChildren = categories.filter(
-    c => immediateChildren(c, tree).length == 0
-  );
-  const toOption = (c: CategoryModel) => ({
-    value: c.id,
-    label: getNameWithAncestors(c, tree),
-  });
-  const options = [
-    {
-      label: 'Most Frequently Used',
-      options: mostFrequent.slice(0, MAX_MOST_FREQUENT).map(toOption),
-    },
-    {
-      label: 'Children Categories',
-      options: categoriesWithoutChildren.map(toOption),
-    },
-    {
-      label: 'All Categories',
-      options: categories.map(toOption),
-    },
-  ];
+  const {control} = useFormContext<TransactionFormSchema>();
   return (
-    <div className="col-span-6">
-      <label
-        className="block text-sm font-medium text-gray-700"
-        htmlFor="categoryId"
-      >
-        Category
-      </label>
-      <Controller
-        name="expense.categoryId"
-        control={control}
-        render={({field}) => (
-          <ReactSelect
-            instanceId="categoryId"
-            styles={undoTailwindInputStyles()}
-            options={options}
-            isDisabled={field.disabled}
-            {...field}
-            value={
-              field.value
-                ? toOption(mustFindCategory(field.value, categories))
-                : null
-            }
-            onChange={newValue => field.onChange(newValue!.value)}
-          />
-        )}
-      />
-    </div>
+    <FormField
+      control={control}
+      name="expense.categoryId"
+      render={({field}) => (
+        <FormItem className="col-span-6">
+          <FormLabel>Category</FormLabel>
+          <FormControl>
+            <CategorySelect value={field.value} onChange={field.onChange} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
   );
+}
+
+function CategorySelect({
+  value,
+  onChange,
+}: {
+  value: number;
+  onChange: (id: number) => void;
+}) {
+  const {getValues} = useFormContext<TransactionFormSchema>();
+  const [optionsOpen, setOptionsOpen] = useState(false);
+  const {categories} = useAllDatabaseDataContext();
+  const vendor = getValues('expense.vendor') ?? '';
+  const matchesVendorIfAny = useCallback(
+    (t: Transaction) => !vendor || (isExpense(t) && t.vendor == vendor),
+    [vendor]
+  );
+  const tree = useMemo(() => makeCategoryTree(categories), [categories]);
+  const groups = useOptions({transactionFilter: matchesVendorIfAny, tree});
+  return (
+    <Popover modal={true} open={optionsOpen} onOpenChange={setOptionsOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          className={cn(
+            'w-full justify-between',
+            // TODO: remove as it's always false.
+            !value && 'text-muted-foreground'
+          )}
+        >
+          {getNameWithAncestors(mustFindCategory(value, categories), tree)}
+          <ChevronUpDownIcon className="ml-2 h-5 w-5 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className="max-h-[--radix-popover-content-available-height] w-[--radix-popover-trigger-width] p-0"
+        side="bottom"
+      >
+        <Command>
+          <CommandInput placeholder="Search category..." />
+          <CommandList>
+            <CommandEmpty>No category found.</CommandEmpty>
+            {groups.map(group => (
+              <CommandGroup key={group.label} heading={group.label}>
+                {group.options.map(c => (
+                  <CommandItem
+                    key={c.id}
+                    value={c.fullName}
+                    onSelect={() => {
+                      onChange(c.id);
+                      setOptionsOpen(false);
+                    }}
+                  >
+                    <CheckIcon
+                      className={cn(
+                        'mr-2 h-4 w-4',
+                        value === c.id ? 'opacity-100' : 'opacity-0'
+                      )}
+                    />
+                    {c.fullName}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            ))}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+type CategoryOptions = Array<{
+  label: string;
+  options: Array<{id: number; fullName: string}>;
+}>;
+
+function useOptions({
+  transactionFilter: transactionMatch,
+  tree,
+}: {
+  transactionFilter: (t: Transaction) => boolean;
+  tree: CategoryTree;
+}): CategoryOptions {
+  const {categories, transactions} = useAllDatabaseDataContext();
+  const groups = useMemo(() => {
+    const mostFrequentIds = mostFrequentCategories(
+      transactions,
+      transactionMatch
+    );
+    const mostFrequent = mostFrequentIds.map(id =>
+      mustFindCategory(id, categories)
+    );
+    const categoriesWithoutChildren = categories.filter(
+      c => immediateChildren(c, tree).length === 0
+    );
+    const toOption = (c: CategoryModel) => ({
+      id: c.id,
+      fullName: getNameWithAncestors(c, tree),
+    });
+    return [
+      {
+        label: 'Most Frequently Used',
+        options: mostFrequent.slice(0, MAX_MOST_FREQUENT).map(toOption),
+      },
+      {
+        label: 'Children Categories',
+        options: categoriesWithoutChildren.map(toOption),
+      },
+      {
+        label: 'All Categories',
+        options: categories.map(toOption),
+      },
+    ];
+  }, [categories, transactions, transactionMatch, tree]);
+  return groups;
 }
 
 function mostFrequentCategories(
   allTransactions: Transaction[],
-  vendor: string
+  matchingFilter: (t: Transaction) => boolean
 ): number[] {
   const expenses = allTransactions.filter(isExpense);
-  const matching = expenses.filter(x => !vendor || x.vendor == vendor);
+  const matching = expenses.filter(matchingFilter);
   const now = new Date();
   const matchingRecent = matching.filter(
     x => differenceInMonths(now, x.timestampEpoch) <= 3
