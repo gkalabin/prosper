956b118453232cb7e2d7fa67665d9e824e4d2260
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-08-14 14:50:21 +0100

    Use larger text to avoid zooming in on ios

--- src/components/ui/html-select.tsx
+++ src/components/ui/html-select.tsx
@@ -10,7 +10,7 @@ const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
     return (
       <select
         className={cn(
-          'border-Select flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm ring-offset-background',
+          'border-Select flex h-10 w-full rounded-md border bg-background px-3 py-2 ring-offset-background',
           'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
           'disabled:cursor-not-allowed disabled:opacity-50',
           className
