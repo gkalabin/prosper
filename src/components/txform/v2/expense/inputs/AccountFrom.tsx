b5d4e3538c1e4dda18e9dcf0d45ee54776bf4c92
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-08-16 23:33:16 +0100

    Move Account input to a shared component

--- src/components/txform/v2/expense/inputs/AccountFrom.tsx
+++ src/components/txform/v2/expense/inputs/AccountFrom.tsx
@@ -1,64 +1,32 @@
 import {useSharingType} from '@/components/txform/v2/expense/useSharingType';
+import {Account} from '@/components/txform/v2/shared/Account';
 import {TransactionFormSchema} from '@/components/txform/v2/types';
 import {Button} from '@/components/ui/button';
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import {Select} from '@/components/ui/html-select';
-import {useAllDatabaseDataContext} from '@/lib/context/AllDatabaseDataContext';
-import {useDisplayBankAccounts} from '@/lib/model/AllDatabaseDataModel';
-import {fullAccountName} from '@/lib/model/BankAccount';
 import {useFormContext} from 'react-hook-form';
 
 export function AccountFrom() {
   const {formState, setValue, control} =
     useFormContext<TransactionFormSchema>();
   const {paidSelf} = useSharingType();
-  const accounts = useDisplayBankAccounts();
-  const {banks} = useAllDatabaseDataContext();
   if (!paidSelf) {
     return <></>;
   }
   return (
-    <FormField
-      control={control}
-      name="expense.accountId"
-      render={({field}) => (
-        <FormItem className="col-span-6">
-          <FormLabel>I paid from</FormLabel>
-          <FormControl>
-            <Select
-              {...field}
-              value={field.value?.toString()}
-              onChange={e => field.onChange(parseInt(e.target.value, 10))}
-            >
-              {accounts.map(x => (
-                <option key={x.id} value={x.id}>
-                  {fullAccountName(x, banks)}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
-          <FormMessage />
-          <div className="text-xs">
-            or{' '}
-            <Button
-              type="button"
-              onClick={() => setValue('expense.sharingType', 'PAID_OTHER_OWED')}
-              variant="link"
-              size="inherit"
-              disabled={formState.isSubmitting}
-            >
-              someone else paid for this expense
-            </Button>
-            .
-          </div>
-        </FormItem>
-      )}
-    />
+    <>
+      <Account fieldName="expense.accountId" label="I paid from" />
+      <div className="col-span-6 -mt-3 text-xs">
+        or{' '}
+        <Button
+          type="button"
+          onClick={() => setValue('expense.sharingType', 'PAID_OTHER_OWED')}
+          variant="link"
+          size="inherit"
+          disabled={formState.isSubmitting}
+        >
+          someone else paid for this expense
+        </Button>
+        .
+      </div>
+    </>
   );
 }
--- src/components/txform/v2/income/IncomeForm.tsx
+++ src/components/txform/v2/income/IncomeForm.tsx
@@ -1,4 +1,3 @@
-import {AccountTo} from '@/components/txform/v2/income/inputs/AccountTo';
 import {Amount} from '@/components/txform/v2/income/inputs/Amount';
 import {Category} from '@/components/txform/v2/income/inputs/Category';
 import {Companion} from '@/components/txform/v2/income/inputs/Companion';
@@ -7,13 +6,14 @@ import {OwnShareAmount} from '@/components/txform/v2/income/inputs/OwnShareAmoun
 import {Payer} from '@/components/txform/v2/income/inputs/Payer';
 import {SplitTransactionToggle} from '@/components/txform/v2/income/inputs/SplitTransactionToggle';
 import {Tags} from '@/components/txform/v2/income/inputs/Tags';
+import {Account} from '@/components/txform/v2/shared/Account';
 import {Timestamp} from '@/components/txform/v2/shared/Timestamp';
 
 export function IncomeForm() {
   return (
     <>
       <Timestamp fieldName="income.timestamp" />
-      <AccountTo />
+      <Account fieldName="income.accountId" label="Money received to" />
       <SplitTransactionToggle />
       <Companion />
       <Amount />
