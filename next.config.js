7a0226ea03d770ee35cb01a84b70961e367278ed
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-09-27 20:14:53 +0100

    Specify allowed origins in the config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: [process.env.PUBLIC_APP_HOST].filter(x => x != undefined),
+    },
+  },
 };
 
 module.exports = nextConfig;
