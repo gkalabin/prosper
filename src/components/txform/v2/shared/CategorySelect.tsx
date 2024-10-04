d0891294b998644904f6a96d9bd605fa959af67e
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-10-04 10:08:40 +0100

    Match height of Tags/Category select

--- src/components/txform/v2/shared/CategorySelect.tsx
+++ src/components/txform/v2/shared/CategorySelect.tsx
@@ -45,7 +45,7 @@ export function CategorySelect({
           type="button"
           variant="outline"
           role="combobox"
-          className="w-full justify-between p-2 text-base font-normal"
+          className="h-10 w-full justify-between p-2 text-base font-normal"
           disabled={disabled}
         >
           {getNameWithAncestors(mustFindCategory(value, categories), tree)}
