08875206019a8389e5868d1eefc602b5d44daa0e
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-10-24 12:41:25 +0100

    ExcludedCategoriesSelector to shadcn components

--- src/app/(authenticated)/stats/ExcludedCategoriesSelector.tsx
+++ src/app/(authenticated)/stats/ExcludedCategoriesSelector.tsx
@@ -1,14 +1,8 @@
 'use client';
-import {undoTailwindInputStyles} from '@/components/forms/Select';
+import {MultiSelect} from '@/components/MultiSelect';
+import {Label} from '@/components/ui/label';
 import {useAllDatabaseDataContext} from '@/lib/context/AllDatabaseDataContext';
-import {
-  Category,
-  CategoryTree,
-  getNameWithAncestors,
-  makeCategoryTree,
-  mustFindCategory,
-} from '@/lib/model/Category';
-import Select from 'react-select';
+import {getNameWithAncestors, makeCategoryTree} from '@/lib/model/Category';
 
 export function ExcludedCategoriesSelector({
   excludedIds,
@@ -18,56 +12,19 @@ export function ExcludedCategoriesSelector({
   setExcludedIds: (newValue: number[]) => void;
 }) {
   const {categories} = useAllDatabaseDataContext();
-  return (
-    <ExcludedCategoriesSelectorStandalone
-      excludedIds={excludedIds}
-      setExcludedIds={setExcludedIds}
-      allCategories={categories}
-    />
-  );
-}
-
-export function ExcludedCategoriesSelectorStandalone({
-  excludedIds,
-  setExcludedIds,
-  allCategories: all,
-}: {
-  excludedIds: number[];
-  setExcludedIds: (newValue: number[]) => void;
-  allCategories: Category[];
-}) {
-  const value: Category[] = excludedIds.map(id => mustFindCategory(id, all));
-  const setValue = (v: readonly Category[]) => setExcludedIds(collectIds(v));
-  const tree = makeCategoryTree(all);
-  const formatter = makeCategoryNameFormatter(tree);
+  const tree = makeCategoryTree(categories);
+  const categoryOptions = categories.map(c => ({
+    value: c.id,
+    label: getNameWithAncestors(c, tree),
+  }));
   return (
     <div>
-      <label
-        htmlFor="categoryIds"
-        className="block text-sm font-medium text-gray-700"
-      >
-        Categories to exclude
-      </label>
-      <Select
-        instanceId="excludeCategoryIdsInStats"
-        styles={undoTailwindInputStyles()}
-        options={[...all]}
-        getOptionLabel={formatter}
-        getOptionValue={formatter}
-        isMulti
-        value={value}
-        onChange={setValue}
+      <Label>Categories to exclude</Label>
+      <MultiSelect
+        options={categoryOptions}
+        value={excludedIds}
+        onChange={setExcludedIds}
       />
     </div>
   );
-}
-
-type CategoryNameFormatter = (c: Category) => string;
-
-function makeCategoryNameFormatter(tree: CategoryTree): CategoryNameFormatter {
-  return (c: Category): string => getNameWithAncestors(c, tree);
-}
-
-function collectIds(categories: readonly Category[]): number[] {
-  return categories.map(c => c.id);
 }
