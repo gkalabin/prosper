3ec38896476cda5dc175bd83a5d62b66421a7001
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-11-18 23:42:21 +0000

    Allow admin API in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,12 @@ import {NextResponse} from 'next/server';
 const FORBIDDEN = new NextResponse(null, {status: 403});
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
+  if (request.nextUrl.pathname == '/api/_ratesz') {
+    // Proceed with the request for the admin handlers.
+    // The admin handlers have their own auth mechanism and not visible from the wider internet, but only from the same server.
+    return NextResponse.next();
+  }
+
   // Do not interfere with sign-out flow, allow it straight away.
   if (request.nextUrl.pathname == SIGN_OUT_URL) {
     return NextResponse.next();
