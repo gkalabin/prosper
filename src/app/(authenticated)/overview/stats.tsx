dd6e26772928ff5be2d8d282717d03d528258fec
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-12-11 22:23:06 +0000

    Revamp the stats widget on the overview page

--- src/app/(authenticated)/overview/stats.tsx
+++ src/app/(authenticated)/overview/stats.tsx
@@ -1,10 +1,17 @@
 'use client';
 import {useHideBalancesContext} from '@/app/(authenticated)/overview/context/hide-balances';
 import {accountsSum} from '@/app/(authenticated)/overview/modelHelpers';
+import {Card, CardContent} from '@/components/ui/card';
+import {AmountWithCurrency} from '@/lib/AmountWithCurrency';
 import {useAllDatabaseDataContext} from '@/lib/context/AllDatabaseDataContext';
 import {useDisplayCurrency} from '@/lib/context/DisplaySettingsContext';
-import {accountUnit} from '@/lib/model/BankAccount';
-import {isCurrency} from '@/lib/model/Unit';
+import {
+  isExpense,
+  isIncome,
+  Transaction,
+} from '@/lib/model/transaction/Transaction';
+import {differenceInDays} from 'date-fns';
+import {useExchangedTransactions} from '../stats/modelHelpers';
 
 export function StatsWidget() {
   const displayCurrency = useDisplayCurrency();
@@ -21,28 +28,61 @@ export function StatsWidget() {
   if (!total || hideBalances) {
     return <></>;
   }
-  const totalCash = accountsSum(
-    bankAccounts.filter(a => isCurrency(accountUnit(a, stocks))),
-    displayCurrency,
-    exchange,
-    transactions,
-    stocks
-  );
   return (
-    <div className="rounded border">
-      <h2 className="bg-indigo-300 p-2 text-2xl font-medium text-gray-900">
-        Total {total.format()}
-      </h2>
-      <div className="grid grid-cols-2 px-3 py-2">
-        {totalCash && (
-          <>
-            <span className="text-lg font-medium">Cash</span>{' '}
-            {totalCash.format()}
-            <span className="text-lg font-medium">Equity</span>{' '}
-            {total.subtract(totalCash).format()}
-          </>
-        )}
+    <>
+      <div className="py-5">
+        <div className="text-2xl font-bold">Your total balance</div>
+        <div className="text-3xl font-bold">
+          <div className="inline-block bg-gradient-to-r from-indigo-600 via-purple-500 via-55% to-orange-400 bg-clip-text text-transparent">
+            {total.round().format()}
+          </div>
+        </div>
       </div>
-    </div>
+      <Last30DaysIncomeExpense />
+    </>
+  );
+}
+
+export function Last30DaysIncomeExpense() {
+  const {transactions} = useAllDatabaseDataContext();
+  const now = Date.now();
+  const last30days = (t: Transaction) =>
+    differenceInDays(now, t.timestampEpoch) <= 30;
+  const {input, failed} = useExchangedTransactions(
+    transactions.filter(t => isIncome(t) || isExpense(t)).filter(last30days)
+  );
+  if (failed.length > 0) {
+    return <></>;
+  }
+  let expense = AmountWithCurrency.zero(input.currency());
+  for (const {ownShare} of input.expenses()) {
+    expense = expense.add(ownShare);
+  }
+  let income = AmountWithCurrency.zero(input.currency());
+  for (const {ownShare} of input.income()) {
+    income = income.add(ownShare);
+  }
+  return (
+    <Card>
+      <CardContent className="py-0">
+        <div className="flex flex-row justify-center">
+          <div className="flex grow flex-col items-center gap-1 p-1">
+            <div className="text-sm font-medium text-muted-foreground">
+              Expense
+            </div>
+            <div className="text-lg font-medium">
+              {expense.round().format()}
+            </div>
+          </div>
+          <div className="w-0 border-l">&nbsp;</div>
+          <div className="flex grow flex-col items-center gap-1 p-1">
+            <div className="text-sm font-medium text-muted-foreground">
+              Income
+            </div>
+            <div className="text-lg font-medium">{income.round().format()}</div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
