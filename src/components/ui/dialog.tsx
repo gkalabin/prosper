bbc003c1ae5065b5eedbcac2808e9b9c529f6991
Gregory Kalabin <grigory.kalabin@gmail.com>
2024-12-30 13:13:39 +0000

    Fix modal parts hidden on chome in iOS behind address bar

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -36,7 +36,14 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid max-h-screen w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 overflow-y-scroll border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+        'fixed',
+        // On mobile the dialog takes full view.
+        'left-0 top-0 h-full w-full',
+        // On bigger screen show the dialog in the center of the page.
+        'sm:left-[50%] sm:top-[50%] sm:h-auto sm:max-h-full sm:max-w-md sm:translate-x-[-50%] sm:translate-y-[-50%] sm:rounded-lg sm:shadow-lg',
+        // Animations.
+        'duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+        'z-50 grid content-start gap-4 overflow-y-auto border bg-background p-6',
         className
       )}
       {...props}
