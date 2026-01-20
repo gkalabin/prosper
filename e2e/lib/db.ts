a940dc2a993a057aa52a57f5485e7be10410e352
Gregory Kalabin <grigory.kalabin@gmail.com>
2026-01-20 22:10:42 +0000

    Allow E2E tests to specify DB_URL parts

--- e2e/lib/db.ts
+++ e2e/lib/db.ts
@@ -1,9 +1,7 @@
 import {PrismaClient} from '@prisma/client';
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DB_URL,
-    },
-  },
-});
+const dbUrl =
+  process.env.DB_URL ||
+  `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+export const prisma = new PrismaClient({datasources: {db: {url: dbUrl}}});
